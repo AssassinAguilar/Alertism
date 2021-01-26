45b80552f4cf78700ca6603c52f51bfee354f5a4
AssassinAguilar <70576469+AssassinAguilar@users.noreply.github.com>
2021-01-26 22:03:39 +0530

    Update Alertism.js

--- Alertism.js
+++ Alertism.js
@@ -4,9 +4,8 @@ var css = "\n@import url('https://fonts.googleapis.com/css2?family=Montserrat:wg
 head.appendChild(style), style.type = "text/css", style.styleSheet ? style.styleSheet.cssText = css : style.appendChild(document.createTextNode(css));
 var html = document.createElement("div");
 
-html.innerHTML = "\n<div id='alert-backdrop'><div id='alert-container'><div id='glassmorphism'></div><div id='icon-tick'></div><div id='icon-cross'></div><div id='icon-info'></div><h2 id='alert-h2'></h2><p id='alert-p'></p><input id='alert-input' type='text' autocomplete='off'><div id='btn-container'><button id='cancel-btn'></button><button id='ok-btn'></button></div></div></div>\n",
-	body = document.body || document.getElementsByTagName("body")[0];
-body.appendChild(html);
+html.innerHTML = "\n<div id='alert-backdrop'><div id='alert-container'><div id='glassmorphism'></div><div id='icon-tick'></div><div id='icon-cross'></div><div id='icon-info'></div><h2 id='alert-h2'></h2><p id='alert-p'></p><input id='alert-input' type='text' autocomplete='off'><div id='btn-container'><button id='cancel-btn'></button><button id='ok-btn'></button></div></div></div>\n";
+document.appendChild(html);
 
 var alertBox = document.getElementById("alert-container");
 
