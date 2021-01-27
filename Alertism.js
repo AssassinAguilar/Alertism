d0d76261229c5ec05904e519372f6b5d102cbda2
AssassinAguilar <70576469+AssassinAguilar@users.noreply.github.com>
2021-01-27 10:22:15 +0530

    Update Alertism.js

--- Alertism.js
+++ Alertism.js
@@ -2,7 +2,7 @@ var css = "\n@import url('https://fonts.googleapis.com/css2?family=Montserrat:wg
     head = document.head || document.getElementsByTagName("head")[0],
     style = document.createElement("style");
 head.appendChild(style), style.type = "text/css", style.styleSheet ? style.styleSheet.cssText = css : style.appendChild(document.createTextNode(css));
-div = document.createElement("div");
+var div = document.createElement("div");
 div.setAttribute("id", "alertSuperContainer");
 div.innerHTML = "\n<div id='alert-backdrop'><div id='alert-container'><div id='glassmorphism'></div><div id='icon-tick'></div><div id='icon-cross'></div><div id='icon-info'></div><h2 id='alert-h2'></h2><p id='alert-p'></p><input id='alert-input' type='text' autocomplete='off'><div id='btn-container'><button id='cancel-btn'></button><button id='ok-btn'></button></div></div></div>\n",
 document.querySelector("body").appendChild(div);
