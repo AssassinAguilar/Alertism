5887d8737cf990fc222c78206c20d8a770a4f65f
AssassinAguilar <70576469+AssassinAguilar@users.noreply.github.com>
2021-01-27 12:35:11 +0530

    Update Alertism.js

--- Alertism.js
+++ Alertism.js
@@ -10,7 +10,7 @@ if (style.styleSheet){
 }
 var div = document.createElement('div');
 var html = "\n<div id='alert-backdrop'><div id='alert-container'><div id='glassmorphism'></div><div id='icon-tick'></div><div id='icon-cross'></div><div id='icon-info'></div><h2 id='alert-h2'></h2><p id='alert-p'></p><input id='alert-input' type='text' autocomplete='off'><div id='btn-container'><button id='cancel-btn'></button><button id='ok-btn'></button></div></div></div>\n";
-document.body.appendChild(div);
+document.getElememtById("body").appendChild(div);
 div.appendChild(html);
 
 var alertBox = document.getElementById("alert-container");
